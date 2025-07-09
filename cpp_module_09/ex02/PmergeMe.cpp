72d145b61ada862d040748115ea122e5682b3e95
IDsuroh <roh7891backup@gmail.com>
2025-07-09 15:49:57 +0100

    commit

--- cpp_module_09/ex02/PmergeMe.cpp
+++ cpp_module_09/ex02/PmergeMe.cpp
@@ -80,3 +80,45 @@ void    PmergeMe::mergeInsertVector(std::vector<int>& seq)  {
     for (int i = 0; i < n; ++i)
         seq[i] = firsts[i];
 }
+
+void    PmergeMe::mergeInsertDeque(std::deque<int>& seq)    {
+    int n = seq.size();
+    if (n <= 1)
+        return;
+    if (n == 2) {
+        if (seq[0] > seq[1])
+            std::swap(seq[0], seq[1]);
+        return;
+    }
+    int numPairs = n / 2;
+    std::deque<int> firsts;
+    std::deque<int> seconds;
+    firsts.resize(numPairs + (n % 2));
+    seconds.resize(numPairs);
+
+    for (int i = 0; i < numPairs; ++i)  {
+        int a = seq[2 * i], b = seq[2 * i + 1];
+        if (a <= b) {
+            firsts[i] = a;
+            seconds[i] = b;
+        } else  {
+            firsts[i] = b;
+            seconds[i] = a;
+        }
+    }
+
+    if (n % 2 == 1)
+        firsts.push_back(seq[n - 1]);
+
+    mergeInsertDeque(firsts);
+
+    for (int i = 0; i < numPairs; ++i)  {
+        int value = seconds[i];
+        std::deque<int>::iterator   pos
+            = std::lower_bound(firsts.begin(), firsts.end(), value);
+        firsts.insert(pos, value);
+    }
+
+    for (int i = 0; i < n; ++i)
+        seq[i] = firsts[i];
+}
---
+++ cpp_module_09/ex02/main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <deque>
+#include <cstdlib>
+#include "PmergeMe.hpp"
+
+int main(int argc, char** argv) {
+    if (argc < 2)   {
+        std::cerr << "Error\n";
+        return 1;
+    }
+
+    std::vector<int> vec;
+    std::deque<int> deq;
+    for (int i = 1; i < argc; ++i)  {
+        if (argv[i][0] == '\0') {
+            std::cerr << "Error\n";
+        }
+        int x = std::atoi(argv[i]);
+        if (x <= 0) {
+            std::cerr << "Error\n";
+            return 1;
+        }
+        vec.push_back(x);
+        deq.push_back(x);
+    }
+
+    std::cout << "Before:";
+    for (size_t i = 0; i < vec.size(); ++i)
+        std::cout << " " << vec[i];
+    std::cout << std::endl;
+
+    PmergeMe    sorter;
+    double      tVec = sorter.sortVector(vec);
+    double      tDeq = sorter.sortDeque(deq);
+
+    std::cout << "After:";
+    for (size_t i = 0; i < vec.size(); ++i)
+        std::cout << " " << vec[i];
+    std::cout << std::endl;
+
+    std::cout
+        << "Time to process a range of " << vec.size()
+        << " elements with std::vector : " << tVec << " us\n"
+        << "Time to process a range of " << deq.size()
+        << " elements with std::deque : " << tDeq << " us\n";
+
+    return 0;
+}
